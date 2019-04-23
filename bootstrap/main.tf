0c39a2a82d15ca7b2c74feb4c1e5f57770884174
belitre <miguelterron@gmail.com>
2019-04-23 22:35:03 +0100

    bucket for terraform state in gce

---
+++ bootstrap/main.tf
@@ -0,0 +1,12 @@
+provider "google" {
+  region      = "${var.tf_state_region}"
+  project     = "${var.project}"
+}
+
+resource "google_storage_bucket" "terraform-bucket" {
+  name        = "${var.tf_bucket_name}"
+  location    = "${var.tf_bucket_location}"
+  versioning  = {
+    enabled = true
+  }
+}
