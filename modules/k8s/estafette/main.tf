5cf29cb1cf5db7b26a8bbd0c7c996fe5281db779
belitre <miguelterron@gmail.com>
2019-05-20 22:33:25 +0100

    removed commented stuff

--- modules/k8s/estafette/main.tf
+++ modules/k8s/estafette/main.tf
@@ -20,15 +20,3 @@ resource "google_project_iam_binding" "estafette_kubernetes_engine_admin_role" {
 resource "google_service_account_key" "estafette_key" {
   service_account_id = "${google_service_account.estafette_service_account.name}"
 }
-
-/*
-resource "kubernetes_secret" "google_application_credentials" {
-  metadata = {
-    name = "${var.service_account_name}"
-    namespace = "${var.namespace}"
-  }
-  data {
-    google-service-account.json = "${base64decode(google_service_account_key.estafette_key.private_key)}"
-  }
-}
-*/
