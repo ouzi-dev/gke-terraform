9978eb5639e042c16c0e01a16cfd56474fc6a6ac
Alex Ouzounis <alex@ouzi.dev>
2019-08-27 13:14:30 +0100

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,15 @@ output "estafette_secret_json" {
   value     = module.k8s_bootstrap.estafette_secret_json
   sensitive = true
 }
+
+
+output "cluster_endpoint" {
+  value     = module.cluster.endpoint
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = module.cluster.cluster_ca_certificate
+  sensitive = true
+}
+
