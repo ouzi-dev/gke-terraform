4ca28d231e5f031b5a412f9bda8dca404b5197d3
Alex Ouzounis <alex@ouzi.dev>
2020-05-07 09:11:11 +0100

    fix: respect cluster autoscaling off flag

--- modules/gke-cluster/main.tf
+++ modules/gke-cluster/main.tf
@@ -40,23 +40,29 @@ resource "google_container_cluster" "k8s-cluster" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  cluster_autoscaling {
-    enabled             = var.cluster_autoscaling
-    autoscaling_profile = var.cluster_autoscaling_profile
-    resource_limits {
-      resource_type = "cpu"
-      minimum       = var.cluster_autoscaling_min_cpu
-      maximum       = var.cluster_autoscaling_max_cpu
-    }
-    resource_limits {
-      resource_type = "memory"
-      minimum       = var.cluster_autoscaling_min_memory
-      maximum       = var.cluster_autoscaling_max_memory
-    }
-    auto_provisioning_defaults {
-      oauth_scopes = var.cluster_autoscaling_gke_scopes
-    }
+  dynamic "cluster_autoscaling" {
+    for_each = var.cluster_autoscaling?[1,]:[] 
+      content {
+        enabled             = var.cluster_autoscaling
+        autoscaling_profile = var.cluster_autoscaling_profile
+        resource_limits {
+          resource_type = "cpu"
+          minimum       = var.cluster_autoscaling_min_cpu
+          maximum       = var.cluster_autoscaling_max_cpu
+        }
+        resource_limits {
+          resource_type = "memory"
+          minimum       = var.cluster_autoscaling_min_memory
+          maximum       = var.cluster_autoscaling_max_memory
+        }
+        auto_provisioning_defaults {
+          oauth_scopes = var.cluster_autoscaling_gke_scopes
+        }
+      }
+    
   }
+
+
 
   maintenance_policy {
     daily_maintenance_window {
